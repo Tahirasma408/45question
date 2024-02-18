eff713223f69289ca74c283333994f597dc1b5c3
Tahirasma408 <119299889+Tahirasma408@users.noreply.github.com>
2024-02-18 22:49:40 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Question 1
+console.log("Hello, World Done");
+// question 2
+var myName = 'Muhammad Tahir';
+var message = 'is a good person';
+console.log(myName + " " + message);
---
+++ main.ts
@@ -0,0 +1,10 @@
+//Question 1
+console.log("Hello, World Done");
+
+// question 2
+let myName = 'Muhammad Tahir';
+let message = 'is a good person';
+console.log(myName+" "+message);
+
+
+
